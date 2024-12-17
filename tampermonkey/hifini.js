e539a1a67ed6a8d8fd606c869d6a231a16e03fa4
MountGao <631595371@qq.com>
2024-12-17 23:26:51 +0800

    hifini: sleep 1s

--- tampermonkey/hifini.js
+++ tampermonkey/hifini.js
@@ -5,150 +5,162 @@
 
     console.log('Hello Hifini')
 
-    const KEY_HREF = 'data-href'
-    const KEY_TID = 'data-tid'
-
-    const musicList = Array.from(document.querySelectorAll(`[${KEY_TID}][${KEY_HREF}]`))
-        .filter((item) => {
-            return item.getAttribute(KEY_TID) && item.getAttribute(KEY_HREF) && !item.classList.contains('top_3')
-        })
-
-    if (musicList.length === 0) return
-
-    const playerEl = document.createElement('div')
-    musicList[0].parentNode.before(playerEl)
-
-    async function loadPlayerAsset(url = '') {
-      return await new Promise((resolve, reject) => {
-        const suffix = url.split('.').at(-1)
-        const tagName = suffix === 'css' ? 'link' : 'script'
-        const el = document.createElement(tagName)
-
-        el.setAttribute('crossorigin', 'anonymous')
-
-        if (tagName === 'link') {
-          el.href = url
-          el.rel = 'stylesheet'
-        } else {
-          el.src = url
-        }
-
-        el.addEventListener('load', () => {
-          resolve(true)
-        })
-
-        el.addEventListener('error', () => {
-          reject(false)
-        })
-
-        document.head.append(el)
-      })
+    const sleep = async (sec = 1000) => {
+        await new Promise((resolve) => { setTimeout(() => resolve(), sec) })
     }
 
-    let [cssLoaded, jsLoaded] = await Promise.all([
-        loadPlayerAsset('https://unpkg.com/aplayer@1.10.1/dist/APlayer.min.css'),
-        loadPlayerAsset('https://unpkg.com/aplayer@1.10.1/dist/APlayer.min.js')
-    ])
-    if (!cssLoaded || !jsLoaded) {
-        [cssLoaded, jsLoaded] = await Promise.all([
-          loadPlayerAsset('https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.css'),
-          loadPlayerAsset('https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js')
-        ])
-    }
-    if (!cssLoaded || !jsLoaded) {
-        [cssLoaded, jsLoaded] = await Promise.all([
-          loadPlayerAsset('https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css'),
-          loadPlayerAsset('https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js')
-        ])
-    }
+    const startWork = async () => {
+        await sleep()
 
-    if (!jsLoaded || !window.APlayer || !cssLoaded) {
-        return playerEl.textContent = 'Load APlayer Failed!!!'
-    }
+        const KEY_HREF = 'data-href'
+        const KEY_TID = 'data-tid'
 
-    const myPlayer = new window.APlayer({
-        container: playerEl,
-        fixed: false,
-        mini: false,
-        autoplay: false,
-        preload: 'auto',
-        volume: 0.5,
-        mutex: true,
-        theme: '#ad7a86',
-        listFolded: false,
-        listMaxHeight: 90,
-    })
+        const musicList = Array.from(document.querySelectorAll(`[${KEY_TID}][${KEY_HREF}]`))
+            .filter((item) => {
+                return item.getAttribute(KEY_TID) && item.getAttribute(KEY_HREF) && !item.classList.contains('top_3')
+            })
 
-    const loadStep = 10
-    let loadIndex = 0
-    let loadDone = false
+        if (musicList.length === 0) return
 
-    const getMusic = (musicDetailLink) => {
-        return new Promise((resolve) => {
-            const iframe = document.createElement('iframe')
-            iframe.src = musicDetailLink
-            iframe.width = '0'
-            iframe.height = '0'
-            iframe.frameborder = '0'
+        const playerEl = document.createElement('div')
+        musicList[0].parentNode.before(playerEl)
 
-            iframe.addEventListener('load', () => {
-                const music = iframe?.contentWindow?.ap4?.music || null
-                if (music) {
-                    resolve(music)
+        async function loadPlayerAsset(url = '') {
+            return await new Promise((resolve, reject) => {
+                const suffix = url.split('.').at(-1)
+                const tagName = suffix === 'css' ? 'link' : 'script'
+                const el = document.createElement(tagName)
+
+                el.setAttribute('crossorigin', 'anonymous')
+
+                if (tagName === 'link') {
+                    el.href = url
+                    el.rel = 'stylesheet'
                 } else {
-                    resolve(null)
+                    el.src = url
                 }
 
-                iframe.remove()
+                el.addEventListener('load', () => {
+                    resolve(true)
+                })
+
+                el.addEventListener('error', () => {
+                    reject(false)
+                })
+
+                document.head.append(el)
             })
-
-            iframe.addEventListener('error', () => {
-                resolve(null)
-                iframe.remove()
-            })
-
-            document.body.append(iframe)
-        }).catch(e => e)
-    }
-
-    const loadList = async () => {
-        for (const item of musicList.slice(loadIndex, loadIndex + loadStep)) {
-            const music = await getMusic(item.getAttribute(KEY_HREF))
-            music && myPlayer.list.add(music)
         }
-        loadIndex += loadStep
-        loadDone = loadIndex >= musicList.length
-    }
 
-    const loadBtn = document.createElement('button')
-    loadBtn.classList.add('btn', 'btn-primary', 'btn-block')
-    loadBtn.style.boxSizing = 'border-box'
-    loadBtn.textContent = '加载更多'
+        let [cssLoaded, jsLoaded] = await Promise.all([
+            loadPlayerAsset('https://unpkg.com/aplayer@1.10.1/dist/APlayer.min.css'),
+            loadPlayerAsset('https://unpkg.com/aplayer@1.10.1/dist/APlayer.min.js')
+        ])
+        if (!cssLoaded || !jsLoaded) {
+            [cssLoaded, jsLoaded] = await Promise.all([
+                loadPlayerAsset('https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.css'),
+                loadPlayerAsset('https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js')
+            ])
+        }
+        if (!cssLoaded || !jsLoaded) {
+            [cssLoaded, jsLoaded] = await Promise.all([
+                loadPlayerAsset('https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css'),
+                loadPlayerAsset('https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js')
+            ])
+        }
 
-    loadBtn.addEventListener('click', async (evt) => {
-        evt.stopPropagation()
+        if (!jsLoaded || !window.APlayer || !cssLoaded) {
+            return playerEl.textContent = 'Load APlayer Failed!!!'
+        }
 
-        loadBtn.disabled = true
-        loadBtn.classList.replace('btn-primary', 'btn-secondary')
+        const myPlayer = new window.APlayer({
+            container: playerEl,
+            fixed: false,
+            mini: false,
+            autoplay: false,
+            preload: 'auto',
+            volume: 0.5,
+            mutex: true,
+            theme: '#ad7a86',
+            listFolded: false,
+            listMaxHeight: 90,
+        })
 
-        if (!loadDone) {
-            loadBtn.textContent = '正在加载...'
+        const loadStep = 10
+        let loadIndex = 0
+        let loadDone = false
 
-            await loadList()
+        const getMusic = (musicDetailLink) => {
+            return new Promise((resolve) => {
+                const iframe = document.createElement('iframe')
+                iframe.src = musicDetailLink
+                iframe.width = '0'
+                iframe.height = '0'
+                iframe.frameborder = '0'
+
+                iframe.addEventListener('load', async () => {
+                    await sleep()
+
+                    const music = iframe?.contentWindow?.ap4?.music || null
+                    if (music) {
+                        resolve(music)
+                    } else {
+                        resolve(null)
+                    }
+
+                    iframe.remove()
+                })
+
+                iframe.addEventListener('error', () => {
+                    resolve(null)
+                    iframe.remove()
+                })
+
+                document.body.append(iframe)
+            }).catch(e => e)
+        }
+
+        const loadList = async () => {
+            for (const item of musicList.slice(loadIndex, loadIndex + loadStep)) {
+                const music = await getMusic(item.getAttribute(KEY_HREF))
+                music && myPlayer.list.add(music)
+            }
+            loadIndex += loadStep
+            loadDone = loadIndex >= musicList.length
+        }
+
+        const loadBtn = document.createElement('button')
+        loadBtn.classList.add('btn', 'btn-primary', 'btn-block')
+        loadBtn.style.boxSizing = 'border-box'
+        loadBtn.textContent = '加载更多'
+
+        loadBtn.addEventListener('click', async (evt) => {
+            evt.stopPropagation()
+
+            loadBtn.disabled = true
+            loadBtn.classList.replace('btn-primary', 'btn-secondary')
 
             if (!loadDone) {
-                loadBtn.textContent = '加载更多'
-                loadBtn.disabled = false
-                loadBtn.classList.replace('btn-secondary', 'btn-primary')
+                loadBtn.textContent = '正在加载...'
+
+                await loadList()
+
+                if (!loadDone) {
+                    loadBtn.textContent = '加载更多'
+                    loadBtn.disabled = false
+                    loadBtn.classList.replace('btn-secondary', 'btn-primary')
+                }
             }
-        }
 
-        if (loadDone) {
-            loadBtn.textContent = '加载完毕'
-        }
-    })
+            if (loadDone) {
+                loadBtn.textContent = '加载完毕'
+            }
+        })
 
-    playerEl.querySelector('.aplayer-list').append(loadBtn)
+        playerEl.querySelector('.aplayer-list').append(loadBtn)
 
-    loadBtn.click()
+        loadBtn.click()
+    }
+
+    window.addEventListener('load', startWork)
 })();
